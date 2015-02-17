f4fa38346ed6233f0622386f9b8f0cfb25e5e331
pselle <pamela.selle@gmail.com>
2015-02-16 22:27:41 -0500

    Put that noise in some HTML plz

--- app.js
+++ app.js
@@ -9,11 +9,6 @@ yaml = require('js-yaml')
 friends = yaml.safeLoad(fs.readFileSync('friends.yml', 'utf-8'))
 
 http.createServer(function (req, res) {
-//  var streams = ['https://gist.githubusercontent.com/nathanbowser/395a49b40425ae9d6f3a/raw/4d2c3e838a6850cde97264e0d67e3f22c02cf5c9/gistfile1.txt',
-//                 'https://gist.githubusercontent.com/nathanbowser/7f62d217227cae90ef7a/raw/182d185ae25cbfb4c3ea6d3e63f6f70dcace4909/gistfile1.txt'].map(function (gist) {
-//    return request(gist).pipe(JSONStream.parse(['lifts', true]))
-//  })
-
   var streams = friends.map(function(f) {
     return request(f.rss).pipe(new FeedParser({}))
   })
@@ -21,11 +16,10 @@ http.createServer(function (req, res) {
 
   var result = es.merge.apply(es.merge, streams)
   var latest = _(result).filter(sinceLastWeek).map(function(post) {
-    return {
-              name: post.title
-    }
+    return "<a href='" + post.link + "'>" + post.title + "</a>, on "
+      + post.pubdate + " from " + post.meta.title + "<br>"
   })
-  latest.pipe(es.stringify()).pipe(res)
+  latest.pipe(res)
 }).listen(3000)
 
 function sinceLastWeek(post) {
